77d1784b58a82f7ec1b2c91c9393cca725530ca8
Ryan Smith <smith.ryan4694@gmail.com>
2026-01-13 18:20:13 -0500

    data folder

---
+++ schema.sql
@@ -0,0 +1,17 @@
+PRAGMA foreign_keys=ON;
+
+CREATE TABLE departments(
+	department_id TEXT PRIMARY KEY,
+	department_name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE grants(
+	grant_id TEXT PRIMARY KEY,
+	funding TEXT,
+	sponsor TEXT,
+	awardee TEXT,
+	title TEXT,
+	date TEXT,
+	department_id TEXT NOT NULL
+	FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
